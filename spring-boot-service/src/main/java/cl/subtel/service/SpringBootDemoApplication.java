c4b22f29a8faebf719b55599c87fa70617c0da2b
Octavio Cárdenas <ocardenas@subtel.gob.cl>
2019-08-29 16:14:07 -0400

    add

---
+++ spring-boot-service/src/main/java/cl/subtel/service/SpringBootDemoApplication.java
@@ -0,0 +1,20 @@
+package cl.subtel.service;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class SpringBootDemoApplication {
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SpringBootDemoApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+}
