7e472edf7a5eabca2cdd40be6dfd317f2a35f518
Octavio Cárdenas <ocardenas@subtel.gob.cl>
2019-08-30 14:28:55 -0400

    add

--- spring-boot-service/src/main/java/cl/subtel/application/SpringBootApp.java
+++ spring-boot-service/src/main/java/cl/subtel/application/SpringBootApp.java
@@ -2,6 +2,9 @@ package cl.subtel.application;
 
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -12,18 +15,28 @@ import cl.subtel.services.Service;
 import cl.subtel.services.ServiceImpl;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@ComponentScan(basePackages={"cl.subtel.controllers"})
-public class SpringBootApp{
-	
+@ComponentScan(basePackages = { "cl.subtel.controllers" })
+public class SpringBootApp {
+
 	public static void main(String[] args) {
 		String[] beanNames = SpringApplication.run(SpringBootApp.class, args).getBeanDefinitionNames();
 		Arrays.sort(beanNames);
 		Arrays.asList(beanNames).forEach(System.out::println);
 	}
-	
+
 	@Bean
 	public Service getService() {
 		return new ServiceImpl();
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("");
+		dataSource.setUrl("");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("123456");
+		return dataSource;
 	}
 
 }
